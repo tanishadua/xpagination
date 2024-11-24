ffaf7e755aa76072513300cad76f5ed0baddaac6
tanisha dua <tanisha.dua95@gmail.com>
2024-11-24 21:56:45 +0530

    pagination code

--- src/table.jsx
+++ src/table.jsx
@@ -2,7 +2,7 @@ import "./App.css"
 function Table({ data }) {
   const DisplayData = data.map((item) => {
     return (
-      <tr>
+      <tr style={{borderBottom:"1px solid #d8d8d8", textAlign:"left"}}>
         <td>{item.id}</td>
         <td>{item.name}</td>
         <td>{item.email}</td>
@@ -11,17 +11,17 @@ function Table({ data }) {
     );
   });
   return (
-    <div className="App" style={{width:"1000px"}}>
-      <table class="table">
-        <thead>
-          <tr>
+    <div className="App" style={{width:"1000px", height:"500px"}}>
+      <table className="table">
+        <thead style={{borderBottom:"1px solid #d8d8d8", textAlign:"left"}}>
+          <tr style={{height:"50px"}}>
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">Role</th>
           </tr>
         </thead>
-        <tbody>{DisplayData}</tbody>
+        <tbody >{DisplayData}</tbody>
       </table>
     </div>
   );
